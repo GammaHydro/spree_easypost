a6dc7ec4ef4fbff1d27e7d786732d46cab4d7e7c
GammaHydroDev <info@gammahydro.com>
2015-07-14 14:05:14 -0400

    do not purchase shipping label from easypost

--- app/models/spree/shipment_decorator.rb
+++ app/models/spree/shipment_decorator.rb
@@ -1,5 +1,5 @@
 Spree::Shipment.class_eval do
-  state_machine.before_transition :to => :shipped, :do => :buy_easypost_rate
+  #state_machine.before_transition :to => :shipped, :do => :buy_easypost_rate
 
   def tracking_url
     nil # TODO: Work out how to properly generate this
